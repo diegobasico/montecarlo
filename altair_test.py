0237ef225e57455740359a041d57fdc5d6119b8d
diegobasico <diego.ugaz13@gmail.com>
2024-11-03 15:05:39 -0500

    kinda working

---
+++ altair_test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+import altair as alt
+
+
+data = pd.DataFrame({'a': list('CCCDDDEEE'),
+                     'b': [2, 7, 4, 1, 2, 6, 8, 4, 7]})
+chart = alt.Chart(data).mark_bar().encode(
+    x='a',
+    y='average(b)',
+)
+chart
---
+++ mpl_test.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Create a figure containing a single Axes.
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
+plt.show()                           # Show the figure.
